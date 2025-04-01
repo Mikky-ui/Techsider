1c56390eceb0c65a04190608845c14d31308484e
Mikky-ui <60062143+Mikky-ui@users.noreply.github.com>
2025-04-01 14:43:54 +0100

    Initial Navigation Bar with MUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,20 @@ import './App.css'
 import Home from './components/Home'
 import Create from './components/Create'
 import Edit from './components/Edit'
+import Navbar from './components/navigation/navbar'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="" element={<Home/>}/>
-        <Route path="/create" element={<Create/>}/>
-        <Route path="/edit/:id" element={<Edit/>}/>
-      </Routes>
+      <Navbar
+        content = {
+          <Routes>
+            <Route path="" element={<Home/>}/>
+            <Route path="/create" element={<Create/>}/>
+            <Route path="/edit/:id" element={<Edit/>}/>
+          </Routes>
+        }
+      />
     </>
   )
 }
